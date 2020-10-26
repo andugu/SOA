59e6ae9fc2eb4d94cc5d8925171f8b912e0f41ff
aviba2000 <71766756+aviba2000@users.noreply.github.com>
2020-10-26 20:08:46 +0100

    Fork avançat (fins apartat f, no inclòs)

--- ZeOS/sys.c
+++ ZeOS/sys.c
@@ -14,6 +14,8 @@
 #define ESCRIPTURA 1
 
 extern int zeos_ticks;
+extern struct list_head freequeue;
+extern struct list_head readyqueue;
 
 char kernel_buffer[1024];
 
@@ -34,13 +36,69 @@ int sys_getpid()
 	return current()->PID;
 }
 
+void free_user_pages( struct task_struct *task );
+
 int sys_fork()
 {
-  int PID=-1;
+	int PID=-1;
+	if (list_empty(&freequeue)) return -105; /*ENOBUFS*/
+	
+	struct list_head *first_free = list_first(&freequeue);
+	//struct task_struct *pcb = list_head_to_task_struct(first_free);
+	list_del(first_free);
+	union task_union *child;
 
-  // creates the child process
-  
-  return PID;
+	int pag;
+	/*Copy the father's union task_union*/
+	copy_data(current(), child, sizeof(union task_union));
+	
+	/*Copy the father's TP*/
+	allocate_DIR(&(child->task)); //Creating a directory for the child
+	//plantilla de còpia de frames: void set_user_pages( struct task_struct *task ) de mm.c
+	page_table_entry *TP_dad = get_PT(current());
+	page_table_entry *TP_child = get_PT(&(child->task));
+	for (pag=0;pag<NUM_PAG_CODE;pag++) // Code pages are shared (they are read-only)
+	  	TP_child[PAG_LOG_INIT_CODE+pag] = TP_dad[PAG_LOG_INIT_CODE+pag];
+	for (pag=0;pag<NUM_PAG_KERNEL;pag++) // Kernel pages are shared
+	  	TP_child[pag] = TP_dad[pag]; // Kernel starts at 0
+
+
+	int new_ph_pag;
+	for (pag=0;pag<NUM_PAG_DATA;pag++){ //new frames for child's data
+		new_ph_pag=alloc_frame();
+		if (new_ph_pag < 0) {
+			free_user_pages(&(child->task)); //If fork is aborted -> child's frame pages are freed
+			list_add_tail(first_free, &freequeue);
+			return -23; /*ENFILE*/
+		}
+	  	TP_child[PAG_LOG_INIT_DATA+pag].entry = 0;
+	  	TP_child[PAG_LOG_INIT_DATA+pag].bits.pbase_addr = new_ph_pag;
+	  	TP_child[PAG_LOG_INIT_DATA+pag].bits.user = 1;
+	  	TP_child[PAG_LOG_INIT_DATA+pag].bits.rw = 1;
+	  	TP_child[PAG_LOG_INIT_DATA+pag].bits.present = 1;
+	}
+	
+	/*Copy father's data frames' data to child's data frames' data*/
+	//Finding a dad's free logical page
+	unsigned int free_page = -1;
+	for (pag=0; (pag<TOTAL_PAGES) && (free_page<0); pag++) 
+		if (TP_dad[pag].entry == 0) free_page = pag;
+	if (free_page < 0) {
+		free_user_pages(&(child->task));
+		list_add_tail(first_free, &freequeue);
+		return -105;/*Dad has no free pages-> ENOBUFS*/
+	}
+	for (pag=0; pag<NUM_PAG_DATA;pag++){
+		set_ss_pag(TP_dad, free_page, TP_child[PAG_LOG_INIT_DATA+pag].bits.pbase_addr);
+		copy_data((unsigned int*) ((PAG_LOG_INIT_DATA+pag) << 12), (unsigned int*) (free_page << 12), PAGE_SIZE);
+	}
+	
+	del_ss_pag(TP_dad, free_page);
+	set_cr3(current()->dir_pages_baseAddr); //clean dad's TLB
+	
+	//falta apartat f fins el final
+	
+	return PID;
 }
 
 void sys_exit()
